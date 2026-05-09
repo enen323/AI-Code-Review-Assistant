eadcc8132dd785103840a786f6dae13786fc09c2
ASUS <2243409205@qq.com>
2026-05-09 16:48:45 +0800

    a

--- src/main/java/com/ai/code/review/AiCodeReviewAssistantApplication.java
+++ src/main/java/com/ai/code/review/AiCodeReviewAssistantApplication.java
@@ -10,6 +10,5 @@ public class AiCodeReviewAssistantApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AiCodeReviewAssistantApplication.class, args);
-        System.out.println("mvp");
     }
 }
